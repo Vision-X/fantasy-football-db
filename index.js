2014b64d28904fc227f6e8a6e442045c7b0a4f93
Vision-X <michael.s.marlow@gmail.com>
2018-08-20 18:13:30 -0600

    add firebase files and new route for serving data

--- index.js
+++ index.js
@@ -67,6 +67,12 @@ app.get('/rankings', (req, res) => {
   })
 })
 
+app.get('/players', (req, res) => {
+  res.json({
+    data: json
+  })
+})
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log('Listening on port 5000');
